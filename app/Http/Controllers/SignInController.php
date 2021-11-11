adaff58c0ace6ac0719519ee6017e6846b3b550b
Benjamin Odiowa <benco00@yahoo.com>
2021-11-11 12:26:56 +0100

    fixed namespace name

--- app/Http/Controllers/SignInController.php
+++ app/Http/Controllers/SignInController.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
-use \PsychoB\Ethereum\AddressValidator;
+use \Benji\Ethereum\AddressValidator;
 
 class SignInController extends Controller
 {
